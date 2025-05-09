9ce859c1f79f52613ea7bbc536f9c3399b089a6a
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-05-09 17:43:58 +0100

    configured project to use TypeScript

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-import express from "express";
-import dotenv from "dotenv";
+// App setup
 
-dotenv.config();
+import express from "express";
 
 const app = express();
 
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+// Server entry point
+
+import app from "./app.js";
+import config from "./config/config.js";
+
+app.listen(config.port, () => {
+  console.log(`Server is running on http://localhost:${config.port}`);
+});
