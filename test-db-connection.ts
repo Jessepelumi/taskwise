31fb45a4a2e12a7c463f67261a97c12fccf35228
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-05-12 17:39:54 +0100

    function to perform quick database connection test

--- test-db-connection.ts
+++ test-db-connection.ts
@@ -9,10 +9,10 @@ console.log(process.env.DATABASE_URL);
 
 sql`SELECT NOW()`
   .then((res) => {
-    console.log("✅ Connection successful:", res);
+    console.log("Connection successful:", res);
     process.exit(0);
   })
   .catch((err) => {
-    console.error("❌ Connection failed:", err);
+    console.error("Connection failed:", err);
     process.exit(1);
   });
