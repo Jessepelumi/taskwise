7deb9f3cc771d58b400a162f82439f7a9440fa4f
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-05-12 17:43:45 +0100

    modified dotenv configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,11 +5,16 @@ dotenv.config();
 interface Config {
   port: number;
   nodeEnv: string;
+  dbUrl: string;
 }
+
+if (!process.env.DATABASE_URL)
+  throw new Error("DATABASE_URL is not defined in environment variables");
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   nodeEnv: process.env.NODE_ENV || "development",
+  dbUrl: process.env.DATABASE_URL,
 };
 
 export default config;
