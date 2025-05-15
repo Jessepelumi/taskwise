5e86ed396c65cc313f956f9fa42f7c55a7409f9a
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-05-15 15:26:34 +0100

    removed custom global type configuration and created a custom request handler

---
+++ src/types/express.d.ts
@@ -0,0 +1,7 @@
+import { Request } from "express";
+import { users } from "../db/schema/index";
+
+export interface CustomRequest extends Request {
+  user?: typeof users.$inferSelect;
+  userId?: number;
+}
