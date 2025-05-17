70c0acd3d81db5c44574962a461958947994e2e2
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-05-17 15:09:36 +0100

    modified schema to include task update schema

--- src/schema/task.schema.ts
+++ src/schema/task.schema.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+// Tasks can only be assigned to one person
+
 export const createTaskSchema = z.object({
   title: z.string().min(1, { message: "Title is required" }),
   description: z
@@ -20,5 +22,27 @@ export const createTaskSchema = z.object({
     .enum(["todo", "in_progress", "completed"])
     .optional()
     .default("todo"),
+  assignedTo: z.number().optional(),
+});
+
+export const updateTaskSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }).optional(),
+  description: z
+    .string()
+    .min(1, { message: "A description of this task is required" })
+    .optional(),
+  dueDate: z
+    .preprocess(
+      (arg) =>
+        typeof arg === "string" || arg instanceof Date
+          ? new Date(arg)
+          : undefined,
+      z.date()
+    )
+    .refine((date) => !isNaN(date.getTime()), {
+      message: "Invalid due date",
+    })
+    .optional(),
+  status: z.enum(["todo", "in_progress", "completed"]).optional(),
   assignedTo: z.number().optional(),
 });
