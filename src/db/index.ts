d39d6d460f33ba56aa53c60f04b419f817842ace
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-05-17 18:34:59 +0100

    modified dotenv and db connection configuration

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,9 +2,17 @@ import config from "../config/config";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema/index";
+import { lookup as dnsLookup } from "dns";
 
 const connectionString = config.dbUrl;
-const client = postgres(connectionString);
+
+// const client = postgres(connectionString);
+
+const client = postgres(connectionString, {
+  hostname: undefined,
+  lookup: (dnsLookup as any).bind(null),
+} as any);
+
 const db = drizzle(client, { schema });
 
 export default db;
